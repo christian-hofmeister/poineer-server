142a9673fc5307253024eb35c6185956ccc78c15
choxberg <christian.hofmeister@posteo.de>
2025-07-31 12:30:23 +0200

    swagger added

--- POIneer.Server/Program.cs
+++ POIneer.Server/Program.cs
@@ -2,13 +2,34 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using POIneer.Server.Services;
-
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "POIneer API",
+        Version = "v1",
+        Description = "POI download and region management API for the POIneer mobile app"
+    });
+});
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "POIneer API v1");
+        options.RoutePrefix = ""; // Swagger UI unter root (/)
+    });
+}
+
+
 
 var processor = new OsmProcessingService();
 //processor.GenerateDummySQLite("Data/berlin.sqlite");
