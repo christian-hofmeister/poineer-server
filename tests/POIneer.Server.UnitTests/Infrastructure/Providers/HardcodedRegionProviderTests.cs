64d4a1f2e9aff6007ffaff13ccece987b566f85e
Christian Hofmeister <christian.hofmeister@posteo.de>
2026-08-08 10:46:27 +0200

    Add unit tests for HardcodedRegionProvider to validate Berlin region retrieval

--- tests/POIneer.Server.UnitTests/Infrastructure/Providers/HardcodedRegionProviderTests.cs
+++ tests/POIneer.Server.UnitTests/Infrastructure/Providers/HardcodedRegionProviderTests.cs
@@ -1,1 +1,21 @@
+using POIneer.Server.Infrastructure.Providers;
+
 namespace POIneer.Server.UnitTests.Infrastructure.Providers;
+
+public class HardcodedRegionProviderTests
+{
+    [Fact]
+    public void GetRegions_ReturnsBerlinRegion()
+    {
+        // Arrange
+        var sut = new HardcodedRegionProvider();
+
+        // Act
+        var regions = sut.GetRegions();
+
+        // Assert
+        var berlin = Assert.Single(regions, region => region.Id == "berlin");
+
+        Assert.Equal("Berlin", berlin.Name);
+    }
+}
